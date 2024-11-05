0c52e37bbb98043cb09ab5e87686353297e00072
AbdallahMehiz <abdallahmehiz0@gmail.com>
2024-11-05 11:17:22 +0100

    2.0.0?

--- seeker/build.gradle.kts
+++ seeker/build.gradle.kts
@@ -103,7 +103,7 @@ publishing {
     publications {
         create<MavenPublication>("bar") {
             groupId = "com.github.abdallah"
-            version = "0.1.0"
+            version = "2.0.0"
             // Place the path of your artifact here
             artifact("$buildDir/libs/seeker-jvm.jar")
             artifact(tasks["sourceJar"])
